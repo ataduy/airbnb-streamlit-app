5b6bf19326e84745efe722e85556167fd7de8e60
ataduy <111417517+ataduy@users.noreply.github.com>
2025-04-25 20:35:34 +0300

    last

--- Fiyat_Tahmini.py
+++ Fiyat_Tahmini.py
@@ -47,8 +47,8 @@ def fiyattahmini():
     ])
 
     # Özellik mühendisliği
-    price_per_accommodate = 100  # Tahmin yapıldığı için bilinmiyor
-    price_per_bed = 100  # Aynı şekilde bilinmiyor
+    price_per_accommodate = 85  # Tahmin yapıldığı için bilinmiyor
+    price_per_bed = 90  # Aynı şekilde bilinmiyor
 
     # Özellik sözlüğü oluştur
     input_data = {
